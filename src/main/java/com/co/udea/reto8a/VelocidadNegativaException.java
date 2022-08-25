2e95341e546b71e05b0030c1933cfa0ed648a026
Jaime Andrés Muñoz <88641264+andresm05@users.noreply.github.com>
2022-08-24 20:44:24 -0500

    Add files via upload
    Manejo de excepciones para los atributos: Velocidad Máxima y Espesor. Se utiliza la interfaz List con el fin de hacer referencia a abstracciones.

---
+++ src/main/java/com/co/udea/reto8a/VelocidadNegativaException.java
@@ -0,0 +1,15 @@
+package com.co.udea.reto8a;
+
+/**
+ *
+ * @author jaime.munozq
+ */
+public class VelocidadNegativaException extends Exception{
+
+    public VelocidadNegativaException() {
+        super("La velocidad debe ser mayor a 0");
+    }
+    
+    
+    
+}
