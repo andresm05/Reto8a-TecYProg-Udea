650f54bc0d2427f1dea368a60f9cf41d4815e492
Jaime Andrés Muñoz <88641264+andresm05@users.noreply.github.com>
2022-08-24 20:36:32 -0500

    Add files via upload

---
+++ src/main/java/com/co/udea/reto8a/VelocidadException.java
@@ -0,0 +1,15 @@
+package com.co.udea.reto8a;
+
+/**
+ *
+ * @author jaime.munozq
+ */
+public class VelocidadException extends Exception{
+
+    public VelocidadException() {
+        super("La velocidad debe ser mayor a 0");
+    }
+    
+    
+    
+}
